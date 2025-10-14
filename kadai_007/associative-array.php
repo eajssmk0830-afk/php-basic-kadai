eeb1e9672caf3f050760f86fcae77f2a87efac84
eajssmk0830-afk <e.a.jssmk0830@gmail.com>
2025-10-14 12:28:46 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <?php
+  $vegetable_deta = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+  print_r($vegetable_deta);
+  ?>
+
+</body>
+</html>
